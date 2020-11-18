065288b2c361d26af1ab245b091a0f48fc531f5a
Lucas Schell <lucas.schell@acad.pucrs.br>
2020-11-18 20:53:06 -0300

    fix search color

--- FRONT/src/app/app.component.ts
+++ FRONT/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {Component, ViewChild, ViewEncapsulation} from '@angular/core';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
@@ -34,7 +34,8 @@ export interface UserData {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   displayedColumns = ["id", "name", "progress"];
