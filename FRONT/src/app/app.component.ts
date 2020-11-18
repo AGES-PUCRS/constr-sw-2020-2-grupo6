09a70137fe47032cbbe3df689547b97d2c96852d
Lucas Schell <lucas.schell@acad.pucrs.br>
2020-11-18 20:37:47 -0300

    search e mock

--- FRONT/src/app/app.component.ts
+++ FRONT/src/app/app.component.ts
@@ -3,52 +3,30 @@ import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
 
-/*export interface PeriodicElement {
-  professor: string;
-  id: number;
-  formacao: string;
-  turmas: number;
-  turno: string;
-}*/
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+const COLORS = [
+  "uma excelente cadeira",
+  "uma ótima cadeira",
+  "uma cadeira mais ou menos",
+  "uma cadeira boa",
+  "uma cadeira ruim"
 ];
-/*const ELEMENT_DATA: PeriodicElement[] = [
-  {id: 17120004, professor: 'Ana Paula', formacao: 'Tecnologia da Informação', turmas: 8, turno:'Manhã' },
-  {id: 17120005, professor: 'Eduardo', formacao: 'Ciência da Computação', turmas: 7, turno:'Manhã, Noite'},
-  {id: 17120006, professor: 'Silvia', formacao: 'Ciência da Computação', turmas: 10, turno:'Noite'},
-  {id: 17120007, professor: 'Alessandra', formacao: 'Sistemas de Informação', turmas: 12, turno:'Tarde, Noite'},
-  {id: 17120008, professor: 'Isabel', formacao: 'Análise de Dados', turmas: 14, turno:'Manhã, Tarde'},
-  {id: 17120009, professor: 'Sergio', formacao: 'Engenharia da Computação', turmas: 6, turno:'Manhã, Tarde, Noite'},
+const NAMES = [
+  "Fundamentos de programacao",
+  "Programacao paralela",
+  "Ética e cidadania",
+  "Humanismo e cultura religiosa",
+  "Programação baixo nível",
+  "Sistemas digitais",
+  "Matematica Concreta",
+  "Matematica discreta"
+];
 
-];*/
+export interface UserData {
+  id: string;
+  name: string;
+  progress: string;
+  color: string;
+}
 
 /**
  * @title Table with sorting
@@ -58,13 +36,48 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+export class AppComponent {
+  displayedColumns = ["id", "name", "progress"];
+  dataSource: MatTableDataSource<UserData>;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
+  constructor() {
+    // Create 100 users
+    const users: UserData[] = [];
+    for (let i = 1; i <= 100; i++) {
+      users.push(createNewUser(i));
+    }
+
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(users);
+  }
+
+  /**
+   * Set the paginator and sort after the view init since this component will
+   * be able to query its view for the initialized paginator and sort.
+   */
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+}
+
+function createNewUser(id: number): UserData {
+  const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))];
+  const desc = COLORS[Math.round(Math.random() * (COLORS.length - 1))];
+
+  return {
+    id: id.toString(),
+    name: name,
+    progress: desc,
+    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
+  };
 }
